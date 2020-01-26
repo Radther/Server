4fd3340edde779ff43234d2bdc5c32eb327bc9a1
Tom Singleton <tomquil13@gmail.com>
2020-01-26 09:19:11 +0000

    Initial Commit

---
+++ Server/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Server
+//
+//  Created by Tom Singleton on 26/01/2020.
+//  Copyright © 2020 Tom Singleton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
